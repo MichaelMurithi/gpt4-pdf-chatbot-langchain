21c845d6bfd42e48d997dcc72667744117501a8e
Michael Murithi Kariuki <michael.kariuki@siemens-healthineers.com>
2023-06-13 17:33:10 +0200

    feat: cleaned up existing google drive logic

---
+++ utils/google-drive.ts
@@ -0,0 +1,21 @@
+import { google } from "googleapis";
+
+export const getDrive = () => {
+    const auth = new google.auth.GoogleAuth({
+        credentials: {
+            client_email: process.env.GOOGLE_CLIENT_EMAIL,
+            private_key: process.env.GOOGLE_PRIVATE_KEY,
+        },
+        scopes: [
+            'https://www.googleapis.com/auth/drive',
+            'https://www.googleapis.com/auth/drive.appdata',
+            'https://www.googleapis.com/auth/drive.file',
+            'https://www.googleapis.com/auth/drive.metadata',
+            'https://www.googleapis.com/auth/drive.metadata.readonly',
+            'https://www.googleapis.com/auth/drive.photos.readonly',
+            'https://www.googleapis.com/auth/drive.readonly'
+        ],
+    });
+
+    return google.drive({ version: 'v3', auth: auth });
+}

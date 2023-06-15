0fcb63691c253e7e7fd9266f787e6fcaf1bd45d1
Michael Murithi Kariuki <michael.kariuki@siemens-healthineers.com>
2023-06-15 20:23:37 +0200

    feat: added routes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,25 @@
-import ChatResponse from '@/components/chat/ChatResponse';
 import Footer from '@/components/chat/Footer';
-import MessageInput from '@/components/chat/MessageInput';
 import Layout from '@/components/layout';
-import styles from '@/styles/Home.module.css';
-import useChat from '@/utils/use-chat';
+import { useRouter } from 'next/router';
+import Chat from './chat';
+import Documents from './documents';
 
 export default function Home() {
-  const { error, messages, messageListRef } = useChat();
+  const router = useRouter();
 
+  const renderPage = () => {
+    switch (router.pathname) {
+      case '/':
+        return <Chat/>;
+      case '/documents':
+        return <Documents />;
+      default:
+        return <div>Page Not Found</div>;
+    }
+  };
   return (
-      <Layout>
-        <div className="dark mx-auto flex flex-col gap-4">
-          <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
-            Chat with your docs
-          </h1>
-          <main className={styles.main}>
-            <div className={styles.cloud}>
-              <div ref={messageListRef} className={styles.messagelist}>
-              {
-                messages.map((message, index) => {
-                  return <ChatResponse key={`chatResponse-${index}`} message={message} index={index}/>
-                })
-              }
-              </div>
-            </div>
-            <MessageInput/>
-            { error && (
-              <div className="border border-red-400 rounded-md p-4">
-                <p className="text-red-500">{error}</p>
-              </div>
-            )}
-          </main>
-        </div>
+    <Layout>
+      {renderPage()}
       <Footer/>
     </Layout>
   );

74dd2f0fe1827dd8e5211b1bad18779f6f5fc64b
Michael Murithi Kariuki <michael.kariuki@siemens-healthineers.com>
2023-06-17 15:12:18 +0200

    feat: configured navbar

--- components/core/navbar.tsx
+++ components/core/navbar.tsx
@@ -1,16 +1,36 @@
 
 import { NavbarProps } from '@/types/layout';
+import { useSession } from 'next-auth/react';
+import { Avatar } from 'primereact/avatar';
 import { Sidebar } from 'primereact/sidebar';
+import DocumentsList from '../documents/documents-list';
 
 const Navbar = ({ visible, toggleVisibility }: NavbarProps) => {
+  const { data } = useSession();
+
     return (
         <Sidebar
             visible={visible}
             onHide={() => toggleVisibility(false)}
-            closeIcon="pi pi-angle-left"
             closeOnEscape={true}
         >
-          <h2>Sidebar</h2>
+        <div className='flex flex-row justify-between'>
+          <div className='flex flex-row'>
+           <Avatar image={data!.user.image ?? ''} shape="circle" aria-label='User' size='normal' />
+           <p className="ml-2 mt-1">{data?.user.name}</p>
+          </div>
+          <div className='flex flex-row'>
+            <i className="mt-1 m-0 pi pi-cog" aria-label="Settings" />
+            <i className="mt-1 ml-2 m-0 pi pi-angle-left" aria-label="Settings" onClick={() => toggleVisibility(false)}/>
+          </div>
+        </div>
+        <div className='mt-8'>
+          <div className='flex flex-row justify-between'>
+           <h2 className='font-bold'>My documents</h2>
+            <i className="m-0 pi pi-cloud-upload" aria-label="Settings" />
+          </div>
+          <DocumentsList/>
+        </div>
     </Sidebar>
   )
 }
---
+++ components/documents/documents-list.tsx
@@ -0,0 +1,13 @@
+
+const DocumentsList = () => {
+  return (
+    <div className='mt-3'>
+    <p>First document.pdf</p>
+    <p>First document.pdf</p>
+    <p>First document.pdf</p>
+    <p>First document.pdf</p>
+    </div>
+  )
+}
+
+export default DocumentsList

ed3d4b701cbcbcfa52bf612d9c8737795505f217
Michael Murithi Kariuki <michael.kariuki@siemens-healthineers.com>
2023-06-18 14:47:40 +0200

    feat: added file picker package

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,12 @@
 import Footer from '@/components/chat/Footer';
 import Overlays from '@/components/core/Overlays';
 import Layout from '@/components/layout';
-import { useRouter } from 'next/router';
 import Chat from './chat';
 
 export default function Home() {
-	const router = useRouter();
-
-	const renderPage = () => {
-		switch (router.pathname) {
-			case '/':
-				return <Chat />;
-			default:
-				return;
-		}
-	};
 	return (
 		<Layout>
-			{renderPage()}
+			<Chat />
 			<Overlays />
 			<Footer />
 		</Layout>
